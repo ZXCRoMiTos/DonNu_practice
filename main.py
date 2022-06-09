25be5c644971a3114b36de9147dc6a399a39ff62
Danila <kalganovdanila@gmail.com>
2022-06-09 20:42:23 +0300

    update

--- main.py
+++ main.py
@@ -6,29 +6,44 @@ class Vulnerability:
         self.name = name
         self.description = description
         self.rang = rang
-        self.write()
+        self.vulnerabilities[name] = {'description': description, 'rang': rang}
 
     def __str__(self):
         return f"{self.name} | {self.description}"
 
-    def write(self):
-        self.vulnerabilities[self.name] = {'description': self.description, 'rang': self.rang}
-
-    def comparison(self, name):
-        try:
-            another_vulnerability = self.vulnerabilities.get(name)
-            if self.rang > another_vulnerability.get('rang'):
-                print(self.name, 'больше!')
-            elif self.rang < another_vulnerability.get('rang'):
-                print(name, 'больше!')
-            else:
-                print(f'{self.name} и {name} равны!')
-        except AttributeError:
-            print(f'Нет такого имени - {name}, вы можете использовать метод show_all()')
+    def compare_with(self, *args):
+        result = f'Сравнение {self.name}: \n'
+        list_vulnerabilities = list(self.vulnerabilities)
+        for item in args:
+            try:
+                if isinstance(item, int):
+                    num = item - 1
+                    name = list_vulnerabilities[num]
+                else:
+                    name = item
+                try:
+                    row = f'  c {name} -'
+                    another_vulnerability = self.vulnerabilities.get(name)
+                    difference = self.rang - another_vulnerability.get('rang')
+                    if difference > 0:
+                        row += ' менее опасно \n'
+                    elif difference < 0:
+                        row += ' более опасно \n'
+                    else:
+                        row += ' равны по опасности \n'
+                    result += row
+                except AttributeError:
+                    result += f'  Нет уязвимости с именем - {name}, вы можете использовать метод show_all() \n'
+            except IndexError:
+                result += f'  Нет уязвимсоти с id - {item}, вы можете использовать метод show_all() \n'
+        print(result)
 
     def show_all(self):
+        num = 1
         for key, value in self.vulnerabilities.items():
-            print(f'Название: {key} | Описание: {value.get("description")} | Ранг: {value.get("rang")}')
+            print(f'{num} | Название: {key} | Описание: {value.get("description")} | Ранг: {value.get("rang")}')
+            num += 1
+        print()
 
     def up_rang(self, num):
         self.rang += num
@@ -37,6 +52,13 @@ class Vulnerability:
     def down_rang(self, num):
         self.rang -= num
         self.vulnerabilities[self.name]['rang'] -= num
+
+    @staticmethod
+    def help():
+        print('Методы: \n  ~ show_all() - показывает все доступные уязвимости их id, название, описание и ранг \n '
+              ' ~ compare_with() - принимает на вход названия уязвимостей или их id через запятую, возвращает '
+              'результат сравнения \n  ~ up_rang() - принимает на вход целое число и повышает ранг на указанное '
+              'количество \n  ~ down_rang() - принимает на вход целове число и понижает ранг на указанное количество \n')
 
 
 broken_access_control = Vulnerability("Broken Access Control", "Access control enforces policy such that users cannot "
@@ -68,14 +90,18 @@ insecure_design = Vulnerability("Insecure Design", "Insecure design is a broad c
                                                    "risk profiling inherent in the software or system being developed, "
                                                    "and thus the failure to determine what level of security design "
                                                    "is required.", rang=1)
+
+broken_access_control.help()
 broken_access_control.show_all()
-broken_access_control.comparison("Cryptographic Failures")
+
+broken_access_control.compare_with("Cryptographic Failures", "Insecure Design")
+broken_access_control.compare_with(2, 3)
+
+broken_access_control.compare_with("test1", "test2")
+broken_access_control.compare_with(4, 6)
+
 cryptographic_failures.up_rang(2)
-cryptographic_failures.comparison("Broken Access Control")
-cryptographic_failures.up_rang(1)
-broken_access_control.comparison("Cryptographic Failures")
-insecure_design.comparison("Broken Access Control")
-broken_access_control.down_rang(4)
-insecure_design.comparison("Broken Access Control")
-insecure_design.comparison("Несуществующее имя")
-print(insecure_design)
+insecure_design.up_rang(5)
+broken_access_control.compare_with(2, 3)
+
+cryptographic_failures.compare_with(1, 3)
